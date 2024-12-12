a6062c0e539622aeb5d5c2cb4594fbd9da3dd0a9
Simson M <sims91502@gmail.com>
2024-12-12 22:28:05 +0530

    feat: update forum

--- forum-be/controller/forumcontroller.js
+++ forum-be/controller/forumcontroller.js
@@ -4,8 +4,8 @@ import {handleResponse} from '../util/response.js'
 
 export const createForum = async(req,res,body) =>{
     try{
-        const {name,creator,category,description} = body;
-        const newForum = await Forum.create({name,creator,category,description});
+        const {title,creator,category,description} = body;
+        const newForum = await Forum.create({title,creator,category,description});
         handleResponse(res,201,"Created Successfully",null)
     }catch(err){
         console.log(`Error Occured: ${err}`);
@@ -28,6 +28,20 @@ export const getAllForumByCategory = async (req,res)=>{
     try{
         const forums = await Forum.findByCategory(1);
         handleResponse(res,200,"Retrived Successfully",forums)
+    }catch(err){
+        console.log(`Error Occured: ${err}`);
+        handleResponse(res,500,'Service error',null)
+    }
+}
+
+export const updateForumCategory = async (req,res,body)=>{
+    try{
+        const {id,title,category,description} = body;
+        const newforum = await Forum.findByIdAndUpdate(id,{title,category,description})
+        if(!newforum){
+            handleResponse(res,400,'forum not found')
+        }
+        handleResponse(res,200,'Updated Successfully',null)
     }catch(err){
         console.log(`Error Occured: ${err}`);
         handleResponse(res,500,'Service error',null)
--- forum-be/routes/forumRoute.js
+++ forum-be/routes/forumRoute.js
@@ -1,4 +1,4 @@
-import { createForum, getAllForum, getAllForumByCategory } from '../controller/forumcontroller.js'
+import { createForum, getAllForum, getAllForumByCategory, updateForumCategory } from '../controller/forumcontroller.js'
 import { parseJson } from '../util/parseJson.js';
 
 export const forumRoute = async (req, res) => {
@@ -13,6 +13,13 @@ export const forumRoute = async (req, res) => {
         getAllForum(req, res)
     } else if (req.method === 'GET' && req.url === '/api/forum/category') {
         getAllForumByCategory(req, res);
+    } else if (req.method === 'PUT' && req.url === '/api/forum') {
+        try {
+            let body = await parseJson(req);
+            updateForumCategory(req, res, body)
+        } catch (error) {
+            console.log(error);
+        }
     } else {
         res.statusCode = 404
         res.end(JSON.stringify({ "message": "Not found" }))
