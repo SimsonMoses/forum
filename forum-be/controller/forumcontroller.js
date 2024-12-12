228bad057c5e1d965272983eff9e2aa1b8f88421
Simson M <sims91502@gmail.com>
2024-12-12 07:15:18 +0530

    feat: common response added

--- forum-be/controller/forumcontroller.js
+++ forum-be/controller/forumcontroller.js
@@ -1,31 +1,25 @@
 import Forum from '../models/forum.js'
+import {handleResponse} from '../util/response.js'
+
 
 export const createForum = async(req,res,body) =>{
     try{
         const {name,creator} = body;
         const newForum = await Forum.create({name,creator});
-        res.statusCode=201
-        res.write(JSON.stringify({"message":"Created successfully"}))    
-        res.end()
+        handleResponse(res,201,"Created Successfully",null)
     }catch(err){
         console.log(`Error Occured: ${err}`);
-        res.statusCode=500
-        res.write(JSON.stringify({"message":"Service error"}))
-        res.end()
+        handleResponse(res,500,'Service Error',null)
     }
 }
 
 export const getAllForum = async (req,res)=>{
     try {
         const forums = await Forum.find();
-        res.statusCode=200
-        res.write(JSON.stringify(forums))
-        res.end()
+        handleResponse(res,200,"Retrived Successfully",forums)
     } catch (error) {
         console.log(`Error Occured: ${error}`);
-        res.statusCode=500
-        res.write(JSON.stringify({"message":"Service error"}))
-        res.end()
+        handleResponse(res,500,"Service error",null)
     }
 }
 
---
+++ forum-be/util/response.js
@@ -0,0 +1,12 @@
+
+
+export const handleResponse = (res,status,message,data)=>{
+    const response = {
+        status,message,data
+    }
+    res.statusCode=status;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(response));
+}
+
+// todo: error message
