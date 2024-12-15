a3ce3a300865a6123488d933903f7a3ecc99acdf
Simson M <sims91502@gmail.com>
2024-12-15 19:43:31 +0530

    feat: validtion added to all endpoints

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,14 +7,14 @@ export const userRoute = async (req, res) => {
     console.log('user routes log')
     const url = new URL(req.url, `http://${req.headers.host}`)
 
-    if (req.method === 'POST' && req.url === '/api/user/register') {
+    if (req.method === 'POST' && req.url === '/api/user/create') {
         validateToken(req,res,['admin','super_admin'],()=>createUser(req, res));
     } else if (req.method === 'POST' && req.url === '/api/user/login') {
-        loginUser(req, res)
+        await loginUser(req, res)
     } else if (req.method === 'GET' && req.url === '/api/user/me') {
         validateToken(req, res,'', () => me(req, res))
     } else if (req.method === 'GET' && req.url === '/api/user/profile') {
-        validateToken(req, res, '',() => getUserProfile(req, res))
+        validateToken(req, res, 'admin',() => getUserProfile(req, res))
     } else if (req.method === 'PUT' && req.url === '/api/user/profile/update') {
         validateToken(req, res, '',() => updateUser(req, res))
     } else if (req.method === 'GET' && url.pathname === '/api/user/profile/get-all') {
